145dd8a895cd7ba67c8e3262a088eb92a06fa253
ajskilton <aski294@aucklanduni.ac.nz>
2024-06-23 16:40:14 +1200

    refactor: improve GUI layout and add option for generating multiple weeks

--- Python/Gui.py
+++ Python/Gui.py
@@ -3,13 +3,15 @@ from TimeTable   import get_my_timetable
 import arrow
 
 layout = [
-   [sg.T('Enter your calender link')],      
-   [sg.Input(default_text=' ', key='-INPUT-')], 
-   [sg.T('Enter the date you want to start from:')],
-   [sg.Input(default_text='YYYY-M-D HH:mm:ss', key='-DATE_INPUT-')],
-   [sg.CalendarButton('Choose Date', target="-DATE_INPUT-", key='-CALENDAR-')],
-   [sg.Button(button_text="Generate", key='-GENERATE-')]
-   ]
+    [sg.T('Enter your calender link')],      
+    [sg.Input(default_text=' ', key='-INPUT-')], 
+    [sg.T('Enter the date you want to start from:')],
+    [sg.Input(default_text='YYYY-M-D HH:mm:ss', key='-DATE_INPUT-')],
+    [sg.CalendarButton('Choose Date', target="-DATE_INPUT-", key='-CALENDAR-')],
+    [sg.T('Enter the number of weeks you want to generate')],
+    [sg.Input(default_text='6', key='-WEEKS-')],
+    [sg.Button(button_text="Generate", key='-GENERATE-')]
+    ]
 
 # Create the window
 window = sg.Window('ORIGINAL', layout)
@@ -20,9 +22,10 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == "-GENERATE-":
-      print("GENERATING")
-      date = arrow.get(values['-DATE_INPUT-'], 'YYYY-M-D HH:mm:ss').replace(hour=0, minute=0, second=0, microsecond=0, tzinfo='+12:00') 
-      get_my_timetable(date.year, date.month, date.day, 6, values['-INPUT-'])
+        print("GENERATING")
+        date = arrow.get(values['-DATE_INPUT-'], 'YYYY-M-D HH:mm:ss').replace(hour=0, minute=0, second=0, microsecond=0, tzinfo='+12:00') 
+        num_weeks= int(values['-WEEKS-'])
+        get_my_timetable(date.year, date.month, date.day, num_weeks, values['-INPUT-'])
     
 
 window.close()
